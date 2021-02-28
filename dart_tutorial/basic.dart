0b3c75f0a5a62bd2959202a20e37129c165a6e3a
vintageappmaker <adsloader99@gmail.com>
2021-03-01 01:30:09 +0900

    .

--- dart_tutorial/basic.dart
+++ dart_tutorial/basic.dart
@@ -1,3 +1,15 @@
+//  1. main 함수가 최초 실행함수(EntryPoint)
+//  2. 출력함수는 print(리턴값 포함). 종결자는 ;(반드시 사용해야 함)
+//  3. 주석은 //, /**/, ///
+//  4. 강제는 아니지만 들여쓰기는 2칸이 일반적임
+//  5. dart run 파일명 ⇒ 실행
+//  6. dart compile exe ⇒ Windows에서 독립실행파일 생성
+
 void main(List<String> args) {
+  if (args.length > 0) {
+    print(args[0]);
+  }
+
   print("Hello Flutter");
+  print("Hello world");
 }
