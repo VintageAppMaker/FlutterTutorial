c05901e96b76328e03ad8cf0f8a0fb532fed8f8a
VintageAppMaker <adsloader99@gmail.com>
2021-03-05 21:56:44 +0900

    .:

---
+++ dart_tutorial/async_future.dart
@@ -0,0 +1,32 @@
+
+void main(List<String> args) {
+  Future<String> waitAndGo() {
+      return Future.delayed(Duration(seconds: 3), () => '3초 대기');
+    }
+
+    void asyncProcess () async {
+      print('await와 async를 이용한 방법시작');
+      var order = await waitAndGo();
+      print('await의 결과: $order');
+    }
+
+    asyncTest() async {
+      await asyncProcess();
+    }
+
+    asyncTest();
+  
+    Future<int> compute(Function f) {
+      int sum = f(); 
+      return new Future.delayed(new Duration(seconds: 1), () {
+        return sum;
+      });
+    }
+  
+    int myCalculate() => 37 + 45;
+    compute(() => myCalculate()).then(
+      (v){
+        print("계산 후, 1초 대기하며 값을 then()으로 넘김 : sum() is $v");
+      }
+    );
+}
