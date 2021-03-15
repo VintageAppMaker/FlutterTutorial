78b086d0d158fdec8e54744e4ed5d116373eb68c
vintageappmaker <adsloader99@gmail.com>
2021-03-16 00:06:40 +0900

    .

--- lib/basic/LayoutExample.dart
+++ lib/basic/LayoutExample.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class LayoutExample extends StatelessWidget {
   @override
@@ -21,37 +20,84 @@ class LayoutExample extends StatelessWidget {
       children: <Widget>[
         // layout 예제
         buildCenterColumn(),
-        buildCenterRow(), 
-        buildSizedBox()
+        buildCenterRow(),
+        buildSizedBox(),
+
+        Divider(color: Colors.black),
+        buildRow(),
+        buildCenter()
       ],
     );
   }
 
-  SizedBox buildSizedBox() {
-    return SizedBox(
-      width : 100, 
-      height : 100, 
-      child: Container(
-          width: double.infinity, 
-          color: Colors.orange, 
-          child: Text("Sized", textAlign: TextAlign.center, style: TextStyle(fontSize: 30, color: Colors.indigo),)
-        )
+  Center buildCenter() {
+    return Center(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: 130,
+            height: 30,
+            color: Colors.blue,
+          ), //Container
+          Container(
+            width: 125,
+            height: 25,
+            color: Colors.orange,
+          ), //Container
+          Container(
+            height: 20,
+            width: 120,
+            color: Colors.amber,
+          ), //Container
+        ], //<Widget>[]
+      ), //Stack
     );
   }
 
+  Row buildRow() {
+    return Row(children: [
+      Expanded(child: Image.asset("assets/images/title.jpg")),
+      Expanded(
+          flex: 2, child: Text('2배 비율로 공간을 늘립니다', textAlign: TextAlign.center)),
+      Expanded(
+        child: Icon(Icons.access_alarms),
+      )
+    ]);
+  }
+
+  SizedBox buildSizedBox() {
+    return SizedBox(
+        width: 100,
+        height: 35,
+        child: Container(
+            width: double.infinity,
+            color: Colors.orange,
+            child: Text(
+              "Sized",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 28, color: Colors.white),
+            )));
+  }
+
   Center buildCenterColumn() {
-    return Center( child: 
-        Container(color: Colors.amber, width: double.infinity, margin: EdgeInsets.all(16), 
-          child: Column( 
-            children: [ for(int i =0; i< 10; i++) Text("$i") ],
-        )),
-      );
+    return Center(
+      child: Container(
+          color: Colors.amber,
+          width: double.infinity,
+          margin: EdgeInsets.all(16),
+          child: Column(
+            children: [for (int i = 0; i < 10; i++) Text("$i")],
+          )),
+    );
   }
 
   Widget buildCenterRow() {
-    return Container(color: Colors.cyan, width: double.infinity, margin: EdgeInsets.all(6), 
-          child: Row( 
-            children: [ for(int i =0; i< 10; i++) Text("$i") ],
+    return Container(
+        color: Colors.cyan,
+        width: double.infinity,
+        margin: EdgeInsets.all(6),
+        child: Row(
+          children: [for (int i = 0; i < 10; i++) Text("$i")],
         ));
   }
 }
