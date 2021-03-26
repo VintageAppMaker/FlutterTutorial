bd0fd77ef65cba52c73e1e8c2ad5a9eb9f37cbd9
vintageappmaker <adsloader99@gmail.com>
2021-03-26 18:59:47 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MainPageState extends State<MainPage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          buildHeaderBanner("basic", "1. 기본위젯", "빈번하게 사용되는 위젯과 기초지식"),
+          buildHeaderBanner("basic", "1","기본위젯", "빈번하게 사용되는 위젯과 기초지식"),
           buildContentItem(
               context, "/TextExample", "1. Text 예제", "Text 사용법을 정리합니다."),
           buildContentItem(
@@ -90,7 +90,7 @@ class _MainPageState extends State<MainPage> {
           buildContentItem(context, "/CupertinoExample", "14. Cupertino 예제",
               "Cupertino 사용법을 정리합니다."),
           buildHeaderBanner(
-              "Utility", "2. File, Pref, Network", "파일관리 및 네트워크 예제"),
+              "Utility", "2","File, Pref, Network", "파일관리 및 네트워크 예제"),
           buildContentItem2(context, FileIOExample(storage: FileHelper()),
               "15. File I/O 예제", "File Read/Write를 정리합니다."),
           buildContentItem2(context, PrefExample(), "16. Preference 예제",
@@ -103,7 +103,7 @@ class _MainPageState extends State<MainPage> {
               "json 배열처리 플러터 공식예제 2"),
           buildContentItem2(
               context, WebScraperExample(), "20. WebScraper 예제", "공식예제"),
-          buildHeaderBanner("state", "3. 상태관리", "key, Stream, Provider 활용"),
+          buildHeaderBanner("state", "3",  "상태관리", "key, Stream, Provider 활용"),
           buildContentItem2(
               context, KeyExample(), "21. Key 예제", "key에 대한 기초예제"),
           buildContentItem2(context, InWidgetExample(),
@@ -112,7 +112,7 @@ class _MainPageState extends State<MainPage> {
               "InheritedWidget보다 쉽고 강력해진 기능"),
           buildContentItem2(context, StreamBuilderExample(),
               "24. StreamBuilder & HTTP 예제", "StreamBuilder를 이용한 비동기통신 "),
-          buildHeaderBanner("etc", "4. 기타", "기타 위젯 및 기능"),
+          buildHeaderBanner("etc", "4","기타", "기타 위젯 및 기능"),
           buildContentItem2(context, RefreshIndicatorExample(),
               "25. RefreshIndicator 예제", "화면을 pulldown하여 리스트 갱신"),
           buildContentItem2(context, StaggeredExample(),
@@ -175,7 +175,7 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  Widget buildHeaderBanner(String sBanner, String sTitle, String sSubTitle) {
+  Widget buildHeaderBanner(String sBanner, String sNumber, String sTitle, String sSubTitle) {
     return Banner(
       message: sBanner,
       textStyle: TextStyle(color: Colors.red),
@@ -183,34 +183,40 @@ class _MainPageState extends State<MainPage> {
       color: Colors.white,
       child: Container(
         width: double.infinity,
-        color: Colors.red,
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
-          child: Column(
+        child: Card(
+          color: Colors.red,
+          child: Padding( padding: EdgeInsets.all(10), child: Column(
             children: <Widget>[
-              SizedBox(
-                height: 10,
-              ), //SizedBox
-              Text(
+              SizedBox(height: 5,),
+              Row(children: [
+                Expanded(flex: 1, child: SizedBox(width: double.infinity) ),
+                SizedBox(
+                  height: 30,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Text('$sNumber'),
+                  ),
+                )
+              ],),
+              SizedBox(width: double.infinity, child: Text(
                 sTitle,
+                textAlign: TextAlign.end,
                 style: TextStyle(
                     color: Color.fromRGBO(0xFF, 0xFF, 0xFF, 1),
                     fontSize: 24,
                     fontWeight: FontWeight.bold), //TextStyle
-              ),
-              SizedBox(
-                height: 5,
-              ), //SizedBox
-              Text(
+              ),),
+              SizedBox(width: double.infinity, child: Text(
                 sSubTitle,
+                textAlign: TextAlign.end,
                 style: TextStyle(
                     color: Color.fromRGBO(0xFF, 0xFF, 0xF3, 0.8),
                     fontSize: 12,
                     fontWeight: FontWeight.bold), //TextStyle
-              ), //Text
+              ),), //Text
               SizedBox(height: 10),
             ], //<Widget>[]
-          ), //Column
+          )), //Column
         ), //Padding
       ), //Container
     );
