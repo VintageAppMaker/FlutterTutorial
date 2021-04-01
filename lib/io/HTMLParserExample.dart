344dbb21b694468cf3d8f3bf33340e114575ffe3
vintageappmaker <adsloader99@gmail.com>
2021-04-01 23:52:00 +0900

    .

---
+++ lib/io/HTMLParserExample.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:html/parser.dart' as html_parser;
+import 'package:http/http.dart' as http;
+import 'package:html/dom.dart' as dom;
+class HTMLParserExample extends StatefulWidget {
+  @override
+  _HTMLParserExampleState createState() => _HTMLParserExampleState();
+}
+
+
+class _HTMLParserExampleState extends State<HTMLParserExample> {
+
+  List<dom.Element> lst;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  Future getData() async {
+    final response =
+        await http.get(Uri.https('vintageappmaker.tistory.com','' ) );
+    print(response.body);
+    final document = html_parser.parse(response.body);
+
+    setState(() {
+      lst = document.getElementsByClassName('tit_post');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String sTitle = '20. HTMLParser 예제';
+    return MaterialApp(
+      title: sTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(sTitle),
+          ),
+          body: SafeArea(
+              child: lst == null
+                  ? Center(
+                      child:
+                          CircularProgressIndicator(), // Loads Circular Loading Animation
+                    )
+                  : ListView.builder(
+                      itemCount: lst.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(lst[index].text),
+                        );
+                      }
+                    )
+          )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:FlutterTutorial/advance/HorizontalListExample.dart';
 import 'package:FlutterTutorial/advance/RefreshIndicatorExample.dart';
 import 'package:FlutterTutorial/advance/StaggeredGridViewExample.dart';
 import 'package:FlutterTutorial/io/AssetReadExample.dart';
-import 'package:FlutterTutorial/io/WebScraperExample.dart';
+import 'package:FlutterTutorial/io/HTMLParserExample.dart';
 import 'package:FlutterTutorial/route.dart';
 import 'package:FlutterTutorial/steatemanagement/StreamBuilderExample.dart';
 import 'package:flutter/material.dart';
@@ -107,7 +107,7 @@ class _MainPageState extends State<MainPage> {
           buildContentItem2(context, HttpJsonExample(), "19. HTTP, JSON 예제 2",
               "json 배열처리 플러터 공식예제 2"),
           buildContentItem2(
-              context, WebScraperExample(), "20. WebScraper 예제", "공식예제"),
+              context, HTMLParserExample(), "20. HTMLParser 예제", "html 파싱"),
           buildHeaderBanner("state", "3",  "상태관리", "key, Stream, Provider 활용"),
           buildContentItem2(
               context, KeyExample(), "21. Key 예제", "key에 대한 기초예제"),
